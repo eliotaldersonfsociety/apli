b33fbe79781a3bbd47876ba71f433132029ab8aa
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-02-18 11:05:42 -0500

    Create db.js

---
+++ api/config/db.js
@@ -0,0 +1,14 @@
+// config/db.js
+import { Client } from 'pg';
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+client.connect();
+
+export { client };

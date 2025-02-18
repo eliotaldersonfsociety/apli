adb841a023020afa1156a47d8bfaa5559cb25c4f
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-02-18 12:13:37 -0500

    Update login.js

--- api/v1/auth/login.js
+++ api/v1/auth/login.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { body, validationResult } from 'express-validator';
-import { getUserByEmail } from '../models/user';
+import { getUserByEmail } from '../../models/user';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {

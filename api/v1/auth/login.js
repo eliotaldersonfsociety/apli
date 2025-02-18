f3427b54c834cbd6532ef43809ad7a0b3e240336
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-02-18 12:37:54 -0500

    Update login.js

--- api/v1/auth/login.js
+++ api/v1/auth/login.js
@@ -16,21 +16,27 @@ export default async function handler(req, res) {
     }
 
     const { email, password } = req.body;
-    const user = await getUserByEmail(email);
+    try {
+      console.log('Searching for user with email:', email);  // Agrega log para depuración
+      const user = await getUserByEmail(email);
 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+
+      const payload = { id: user.id, email: user.email };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+      return res.status(200).json({ token, user: { id: user.id, name: user.name, lastname: user.lastname, email: user.email } });
+    } catch (error) {
+      console.error('Error during login process:', error.message);  // Log para depuración
+      return res.status(500).json({ message: 'Internal Server Error', error: error.message });
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid password' });
-    }
-
-    const payload = { id: user.id, email: user.email };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    return res.status(200).json({ token, user: { id: user.id, name: user.name, lastname: user.lastname, email: user.email } });
   } else {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
